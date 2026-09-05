3d5c92dd9b8156b06769168e7f7e18ba3c85d134
Dhruv Patel <dhruv.patel.cg@gmail.com>
2026-09-05 10:47:42 +0530

    Add files via upload

--- Python Assignment/Assignment2.py
+++ Python Assignment/Assignment2.py
@@ -1,1 +1,66 @@
+#Q1
+# age = "25"
+# integer_age = int(age)
+# print(integer_age)
+# print(type(integer_age))
 
+#Q2
+# marks = "75.5"
+# float_marks = float(marks)
+# print(float_marks)
+# print(type(float_marks))
+
+#Q3
+# number = 50
+# float_number = float(number)
+# print(float_number)
+# print(type(float_number))
+
+#Q4
+# marks = 85.9
+# integer_marks = int(marks)
+# print(integer_marks)
+
+#Q5
+# roll_number = 101
+# string_roll_number = str(roll_number)
+# print(string_roll_number)
+# print(type(string_roll_number))
+
+#Q6
+# number1 = "18"
+# integer_number1 = int(number1)
+# print(integer_number1)
+# print(type(integer_number1))
+
+# number2 = "92.5"
+# float_number2 = float(number2)
+# print(float_number2)
+# print(type(float_number2))
+
+# number3 = 100
+# string_number3 = str(number3)
+# print(string_number3)
+# print(type(string_number3))
+
+# number4 = 45.8
+# integer_number4 = int(number4)
+# print(integer_number4)
+# print(type(integer_number4))
+
+#Q7
+# 1. 20
+# 2. 10
+# 3. 25
+# 4. <class 'int'>
+# 5. <class 'int'>
+# 6. <class 'str'>
+
+#Q8
+# age = "19"
+# integer_age = int(age)
+# new_age = integer_age + 1
+
+# print("Age:", new_age)
+
+#Q9
