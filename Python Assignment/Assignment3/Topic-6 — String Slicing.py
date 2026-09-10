3b9f496cd53282963f2e9bc6909b4bed49155b88
Dhruv Patel <dhruv.patel.cg@gmail.com>
2026-09-10 15:23:23 +0530

    Topic 6 Assignment 3 completed

---
+++ Python Assignment/Assignment3/Topic-6 — String Slicing.py
@@ -0,0 +1,48 @@
+#Q24
+# text = "PYTHON"
+# PYT
+# THO
+# PYTHON
+
+#Q25
+# text = "PROGRAMMING"
+# PROG
+# RAMMING
+# PROGRAMMING
+
+#Q26
+# text = "COMPUTER"
+# PUTER
+# COMPU
+# MPUT
+
+#Q27
+# text = "PYTHON"
+# PTO
+# YHN
+# NOHTYP
+
+#Q28
+# string = input("Any string:")
+# print(string[-1::-1])
+
+#Q29
+# text = input("A txt:")
+# print(text[::2])
+
+#Q30
+# word = input("Any word:")
+# print(word[0:3])
+# print(word[-1:-4:-1])
+
+#Q31
+# text = "ABCDEFGHIJ"
+# CEG--> START:2, STOP:8, STEP:2
+# IGE--> start:8, stop:2, step:-2
+# JHFDB--> start:-1, stop-11, step:-2
+
+#Q32
+# text = "BTECH-CSE-2026"
+# print(text[:5])
+# print(text[6:9])
+# print(text[-4:])
