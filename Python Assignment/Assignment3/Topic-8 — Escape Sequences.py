b2a4a64bd2bad920ab8753ba21d06318ba5b8ccf
Dhruv Patel <dhruv.patel.cg@gmail.com>
2026-09-10 18:48:00 +0530

    Topic 08 Assignment 3 completed

---
+++ Python Assignment/Assignment3/Topic-8 — Escape Sequences.py
@@ -0,0 +1,26 @@
+#Q42
+# print("Hello\nWorld")
+
+#Q43
+# print("Name:\tRahul")
+# print("Age:\t20")
+# print("City:\tAhmedabad")
+
+#Q44
+# print("C:\Python\Programs") # Can be written like this as python is case sensitive language
+# print("C:\\Python\\Programs") # Asked in question like this
+
+#Q45
+# print("It's Python")
+# print("It\'s Python")
+
+#Q46
+# print('He said "Hello"')
+# print("He said \"Hello\"")
+
+#Q47
+# Python
+# Programming
+
+#Q48
+# print("Student Details\n\nName:\tRahul\nAge:\t20\nCourse:\tB.Tech")
