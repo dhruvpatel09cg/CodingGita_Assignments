498c3dd43c60989a1a6658fa268c99d22685c91a
Dhruv Patel <dhruv.patel.cg@gmail.com>
2026-09-10 20:23:18 +0530

    Topic 10 Assignment 3 completed

---
+++ Python Assignment/Assignment3/Topic-10 — Debugging.py
@@ -0,0 +1,25 @@
+#Q54
+# age = int(input("Enter age: "))
+# print("Age after 5 years:", age + 5)
+
+#Q55
+# print('It\'s Python')
+
+#Q56
+# text = "Python"
+# print(text[1:4])
+
+#Q57
+# a, b = input().split()
+# print(a, b)
+# This program fails as separator is comma.
+
+#Q58
+# This program will print two input numbers next to each other.
+# a, b = input().split()
+
+# print(int(a) + int(b))
+
+#Q59
+# print("C:\\new\\test")
+# python consider \n & \t as a single special-character and prints output accordingly.
