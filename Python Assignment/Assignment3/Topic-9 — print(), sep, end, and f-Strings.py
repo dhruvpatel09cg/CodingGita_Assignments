0403a7e35719b14d0d59421c807e51adbec23a1d
Dhruv Patel <dhruv.patel.cg@gmail.com>
2026-09-10 19:08:53 +0530

    Topic 9 Assignment 3 completed

---
+++ Python Assignment/Assignment3/Topic-9 — print(), sep, end, and f-Strings.py
@@ -0,0 +1,20 @@
+#Q49
+# 2026-09-09
+
+#Q50
+# Hello Python
+
+#Q51
+# print(10,20,30,sep="-",end="\n")
+# print(40,50,60,sep="-")
+
+#Q52
+# name = "Rahul"
+# age = 20
+# city = "Ahemedabad"
+# course = "B.Tech"
+# print(f"Name:\t{name}\nAge:\t{age}\nCity:\t{city}\nCourse:\t{course}")
+
+#Q53
+# price = float(input("Enter the price: "))
+# print(f"Price: {price:.2f}")
