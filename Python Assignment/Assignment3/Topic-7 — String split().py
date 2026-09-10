086c69ab51cf7c0f692de914e925e29d183569b8
Dhruv Patel <dhruv.patel.cg@gmail.com>
2026-09-10 18:22:08 +0530

    Topic 7 Assignment 3 completed

---
+++ Python Assignment/Assignment3/Topic-7 — String split().py
@@ -0,0 +1,50 @@
+#Q33
+# ['Python','is','easy']
+# blank space separates the words.
+
+#Q34
+# ['apple', 'banana', 'mango']
+
+#Q35
+# ['Python is easy']
+# spaces can't be splitted in this as the separator here is ",".
+
+#Q36
+# name = input("Full name:").split()
+# first , last , sur = name
+# print(name)
+# print(first)
+# print(last)
+# print(sur)
+
+#Q37
+# name = input("Your name:").split()
+# first_name , last_name = name
+# print("First name:",first_name)
+# print("Last name:",last_name)
+
+#Q38
+# nums = input("Type any three numbers,separate them with space:").split()
+# num1, num2, num3 = nums
+# print(int(num1)+int(num2)+int(num3))
+
+#Q39
+# student_info = input("Give your Name, Age, Course, City and separate with comma:").split(",")
+# name, age, course, city = student_info
+# print("Name:",name)
+# print("Age:",age)
+# print("Course:",course)
+# print("City:",city)
+
+#Q40
+# email = input("Your email ID:").split("@")
+# username, domain = email
+# print("Username:",username)
+# print("Domain:",domain)
+
+#Q41
+# sentence = input("Any sentence:").split()
+# words = sentence
+# print("First word:",words[0])
+# print("Last word:",words[-1])
+# print("Total words:",len(words))
