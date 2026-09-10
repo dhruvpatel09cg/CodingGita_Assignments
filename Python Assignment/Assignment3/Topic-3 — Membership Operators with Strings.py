63bc5cce213354e150d03283060455012d627460
Dhruv Patel <dhruv.patel.cg@gmail.com>
2026-09-10 11:55:12 +0530

    Topic 3 Assignment 3 completed

---
+++ Python Assignment/Assignment3/Topic-3 — Membership Operators with Strings.py
@@ -0,0 +1,26 @@
+#Q7
+# True
+# False
+# False
+
+#Q8
+# word = "computer"
+# print("p" in word)
+# print("x" in word)
+# print("c" not in word)
+
+#Q9
+# True
+# False
+# True
+# False
+
+# --> Python is a case sensitive language which consider same alphabet in upper case and lower case as different alphabets
+
+#Q10
+# word = input("Any word:")
+# print("a" in word)
+
+#Q11
+# email = input("Your Email ID:")
+# print("@" in email)
