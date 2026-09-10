fee3bb6f4bab9299b3a7943aae6b24103e1d619b
Dhruv Patel <dhruv.patel.cg@gmail.com>
2026-09-10 12:30:12 +0530

    Topic 4 Assignment 3 completed

---
+++ Python Assignment/Assignment3/Topic-4 — ASCII and Unicode.py
@@ -0,0 +1,56 @@
+#Q12
+# print(ord("A"))
+# print(ord("a"))
+# print(ord("Z"))
+# print(ord("z"))
+# print(ord("0"))
+# print(ord("9"))
+# print(ord("@"))
+
+#Q13
+# print(chr(65))
+# print(chr(66))
+# print(chr(97))
+# print(chr(98))
+# print(chr(48))
+# print(chr(57))
+# print(chr(64))
+
+#Q14
+# print(ord("A"))
+# print(ord("a"))
+# print(ord("B"))
+# print(ord("b"))
+# 1) ord("a") is larger
+# 2) The difference is 32
+# 3) Yes the difference is same for B and b
+
+#Q15
+# character = input("Any character:")
+# print(ord(character))
+
+#Q16
+# upper = input("Anu upper case character:")
+# result = ord(upper)
+# result += 1
+# output = chr(result)
+
+# print(output)
+
+#Q17
+# True
+# True
+# True
+# True
+
+# print(ord("A"))
+# print(ord("B"))
+# print(ord("a"))
+# print(ord("b"))
+# print(ord("0"))
+# print(ord("9"))
+
+#Q18
+# print(chr(9731))
+# print(chr(9829))
+# print(chr(8377))
