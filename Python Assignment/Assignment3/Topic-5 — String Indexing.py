11b8ff54ea337d44ba0bebbbe07a43b662268a38
Dhruv Patel <dhruv.patel.cg@gmail.com>
2026-09-10 12:50:35 +0530

    Topic 5 Assignment 3 completed

---
+++ Python Assignment/Assignment3/Topic-5 — String Indexing.py
@@ -0,0 +1,37 @@
+#Q19
+# text = "PYTHON"
+# print(text[0])
+# print(text[1])
+# print(text[-1])
+# print(text[-2])
+
+#Q20
+# text = "COMPUTER"
+# print(text[0])
+# print(text[3])
+# print(text[-1])
+# print(text[-3])
+
+# text = "Python"
+# print(text[0])
+# print(text[3])
+# print(text[-1])
+# print(text[-3])
+
+#Q21
+# P
+# T
+# N
+# O
+
+#Q22
+# word = input("Any word:")
+# print(word[0])
+# print(word[-1])
+
+#Q23
+# word = "PROGRAM"
+# P
+# O
+# M
+# G
