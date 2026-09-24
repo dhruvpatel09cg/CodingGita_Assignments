e18f7635489ce1de7e5c59732c1eb7e02a9fb912
Dhruv Patel <dhruv.patel.cg@gmail.com>
2026-09-24 19:15:56 +0530

    Topic 4 Assignment 04 completed

--- Python Assignment/Assignment4/Topic-4 — Logical Conditions.py
+++ Python Assignment/Assignment4/Topic-4 — Logical Conditions.py
@@ -1,1 +1,54 @@
+# Q29
+# marks = int(input("Enter your marks:"))
+# attend = int(input("Enter your attendance:"))
+# if marks >= 60 and attend >= 75:
+#     print("Eligible")
+# else:
+#     print("Not Eligible")
 
+# Q30
+# marks = int(input("Enter your marks:"))
+# inc = int(input("Enter family income:"))
+# if marks >= 85 or inc <= 300000:
+#     print("Scholarship available")
+# else:
+#     print("No scholarship")
+
+# Q31
+# day = input("Enter Day:")
+# if day == "saturday" or day == "sunday":
+#     print("Weekend")
+# else:
+#     print("Weekday")
+
+# Q32
+# valid_user = "student"
+# valid_pass = "python123"
+# user = input("Enter username:")
+# password = input("Enter password:")
+# if user == valid_user and password == valid_pass:
+#     print("Access Granted")
+# else:
+#     print("Access Denied")
+
+# Q33
+# city = input("Enter city:")
+# if city == "amedabad" or city == "gandhinagar":
+#     print("Delivery Available")
+# else:
+#     print("Delivery Unavailable")
+
+# Q34
+# num = int(input("Enter Integer:"))
+# if 10 <= num <= 50:
+#     print("Inside range")
+# else:
+#     print("Outside range")
+
+# Q35
+# amt = int(input("Enter amount:"))
+# otp = int(input("Enter OTP:"))
+# if amt <= 50000 and otp == 1234:
+#     print("Transaction Approved")
+# else:
+#     print("Transaction Declined")
