26284ce6ada03936b35211d31ce8502362dce555
Dhruv Patel <dhruv.patel.cg@gmail.com>
2026-09-24 18:47:16 +0530

    Topic 3 Assignment 04 completed

--- Python Assignment/Assignment4/Topic-3 — if-elif-else.py
+++ Python Assignment/Assignment4/Topic-3 — if-elif-else.py
@@ -1,1 +1,125 @@
+# Q19
+# marks = int(input("Enter your marks:"))
+# if marks >= 90:
+#     print("A")
+# elif marks >= 80:
+#     print("B")
+# elif marks >= 70:
+#     print("C")
+# elif marks >= 60:
+#     print("D")
+# else:
+#     print("F")
 
+# Q20
+# temp = int(input("Enter temp in Celsius:"))
+# if temp >= 40:
+#     print("Very Hot")
+# elif temp >= 30:
+#     print("Hot")
+# elif temp >= 20:
+#     print("Warm")
+# else:
+#     print("Cold")
+
+# Q21
+# color = input("Color of traffic light:")
+# if color == "red":
+#     print("Stop")
+# elif color == "yellow":
+#     print("Wait")
+# elif color == "green":
+#     print("Go")
+# else:
+#     print("Invalid Signal")
+
+# Q22
+# unit = int(input("Electricity units used:"))
+# if unit <= 100:
+#     print("Low Usage")
+# elif unit <= 300:
+#     print("Medium Usage")
+# elif unit <= 500:
+#     print("High Usage")
+# else:
+#     print("Very High Usage")
+
+# Q23
+# age = int(input("Enter your Age:"))
+# if age < 5:
+#     print("Free Ticket")
+# elif age <= 12:
+#     print("Child Ticket")
+# elif age < 59:
+#     print("Regular Ticket")
+# else:
+#     print("Senior Ticket")
+
+# Q24
+# bmi = float(input("Enter BMI:"))
+# if bmi < 18.5:
+#     print("Underweight")
+# elif bmi <= 24.9:
+#     print("Normal")
+# elif bmi <= 29.9:
+#     print("Overweight")
+# else:
+#     print("Obese")
+
+# Q25
+# month = int(input("Enter month no.:"))
+# if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+#     print("31 Days")
+# elif month == 4 or month == 6 or month == 9 or month == 11:
+#     print("30 Days")
+# elif month == 2:
+#     print("28 or 29 days")
+# else:
+#     print("Invalid month")
+
+# Q26
+# num1 = int(input("No. 1:"))
+# num2 = int(input("No. 2:"))
+# operator = input("Any operator:")
+# if operator == "+":
+#     print(num1 + num2)
+# elif operator == "-":
+#     print(num1 - num2)
+# elif operator == "*":
+#     print(num1 * num2)
+# elif operator == "/":
+#     print(num1 / num2)
+# else:
+#     print("Invalid Operator")
+
+# Q27
+# num = int(input("A no. from 1 to 7:"))
+# if num == 1:
+#     print("Monday")
+# elif num == 2:
+#     print("Tuesday")
+# elif num == 3:
+#     print("Wednesday")
+# elif num == 4:
+#     print("Thursday")
+# elif num == 5:
+#     print("Friday")
+# elif num == 6:
+#     print("Saturday")
+# elif num == 7:
+#     print("Sunday")
+# else:
+#     print("Invalid Day")
+
+# Q28
+# score = int(input("Enter your score:"))
+# if score >= 90:
+#     print("Excellent")
+# elif score >= 75:
+#     print("Very Good")
+# elif score >= 60:
+#     print("Good")
+# elif score >= 40:
+#     print("Average")
+# else:
+#     print("Needs Improvement")
