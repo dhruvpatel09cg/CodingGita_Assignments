c6edc98ca260e58032670caa492aa87a23890dff
Dhruv Patel <dhruv.patel.cg@gmail.com>
2026-09-24 17:07:30 +0530

    Topic 01 Assignment 04 completed

--- Python Assignment/Assignment4/Topic-1 — Basic if Statements.py
+++ Python Assignment/Assignment4/Topic-1 — Basic if Statements.py
@@ -1,1 +1,39 @@
+# Q1
+# integer = int(input("Enter a integer:"))
+# if integer > 0:
+#     print("Positive Number")
 
+# Q2
+# age = int(input("Enter your age:"))
+# if age >= 18:
+#     print("Eligible to Vote")
+
+# Q3
+# temp = int(input("Temperature in celsius:"))
+# if temp > 40:
+#     print("High Temperature")
+
+# Q4
+# num = int(input("Enter a no.:"))
+# if num % 5 == 0:
+#     print("Divisible by 5")
+
+# Q5
+# amount = int(input("Enter bill amount:"))
+# if amount >= 1000:
+#     print("Free Delivery")
+
+# Q6
+# character = input("Enter an Alphabet:")
+# if character == "A":
+#     print("You entered A")
+
+# Q7
+# password = input("Enter Password:")
+# if len(password) >= 8:
+#     print("Strong Length")
+
+# Q8
+# integer = int(input("Enter a number:"))
+# if 100<=integer<=999:
+#     print("Three Digit Number")
