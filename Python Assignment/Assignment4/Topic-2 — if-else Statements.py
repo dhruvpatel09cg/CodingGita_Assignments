0791529b93bfdfbabbe4bac420024225f2a522ac
Dhruv Patel <dhruv.patel.cg@gmail.com>
2026-09-24 18:02:05 +0530

    Topic 2 Assignment 04 completed

--- Python Assignment/Assignment4/Topic-2 — if-else Statements.py
+++ Python Assignment/Assignment4/Topic-2 — if-else Statements.py
@@ -1,1 +1,74 @@
+# Q9
+# num = int(input("Enter a number:"))
+# if num % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
 
+# Q10
+# marks = int(input("Enter your marks:"))
+# if marks >= 40:
+#     print("Pass")
+# else:
+#     print("Fail")
+
+# Q11
+# age = int(input("Enter your age:"))
+# if age >= 18:
+#     print("Adult")
+# else:
+#     print("Minor")
+
+# Q12
+# num = int(input("Enter a no.:"))
+# if num > 0:
+#     print("Positive")
+# else:
+#     print("Non-Positive")
+
+# Q13
+# integer = int(input("Enter a no.:"))
+# if integer % 3 == 0:
+#     print("Divisible by 3")
+# else:
+#     print("Not Divisible by 3")
+
+# Q14
+# correct_password = "python123"
+# password = input("Enter password:")
+# if password == correct_password:
+#     print("Login Successful")
+# else:
+#     print("Invalid Password")
+
+# Q15
+# valid_user = "admin"
+# user = input("Enter username:")
+# if user == valid_user:
+#     print("Welcome Admin")
+# else:
+#     print("Invalid Username")
+
+# Q16
+# num1 = int(input("No. 1:"))
+# num2 = int(input("No. 2:"))
+# if num1 > num2:
+#     print(num1)
+# elif num2 > num1:
+#     print(num2)
+# else:
+#     print("Both are equal")
+
+# Q17
+# temp = int(input("Enter temperature in Celsius:"))
+# if temp > 30:
+#     print("Hot")
+# else:
+#     print("Comfortable")
+
+# Q18
+# amt = int(input("Bill amount:"))
+# if amt >= 5000:
+#     print("Discount available")
+# else:
+#     print("No Discount")
