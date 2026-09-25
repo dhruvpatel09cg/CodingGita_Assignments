96d3ae43a3e8a7541652de539f70d19f0c2b3700
Dhruv Patel <dhruv.patel.cg@gmail.com>
2026-09-25 11:47:43 +0530

    Topic 7 Assignment 04 completed

--- Python Assignment/Assignment4/Topic-7 — match-case.py
+++ Python Assignment/Assignment4/Topic-7 — match-case.py
@@ -1,1 +1,135 @@
+# Q50
+# num = int(input("Option no.:"))
+# match num:
+#     case 1:
+#         print("Add")
+#     case 2:
+#         print("View") 
+#     case 3:
+#         print("Update")
+#     case 4:
+#         print("Delete")
+#     case _:
+#         print("Invalid Choice")
 
+# Q51
+# num = int(input("Day no.:"))
+# match num:
+#     case 1:
+#         print("Monday")
+#     case 2:
+#         print("Tuesday")
+#     case 3:
+#         print("Wednesday")
+#     case 4:
+#         print("Thursday")
+#     case 5:
+#         print("Friday")
+#     case 6:
+#         print("Saturday")
+#     case 7:
+#         print("Sunday")
+#     case _:
+#         print("Invalid Day")
+
+# Q52
+# num1 = int(input("No. 1:"))
+# num2 = int(input("No. 2:"))
+# operator = input("Any operator:")
+# match operator:
+#     case "+":
+#         print(num1 + num2)
+#     case "-":
+#         print(num1 - num2)
+#     case "*":
+#         print(num1 * num2)
+#     case "/":
+#         print(num1/ num2)
+#     case _:
+#         print("Invalid Operator")
+
+# Q53
+# color = input("Signal colour:")
+# match color:
+#     case "red":
+#         print("Stop")
+#     case "green":
+#         print("Go")
+#     case "yellow":
+#         print("Wait")
+#     case _:
+#         print("Invalid Signal")
+
+# Q54
+# grade = input("Your Grade:")
+# match grade:
+#     case "A":
+#         print("Excellent Performance")
+#     case "B":
+#         print("Very Good Performance")
+#     case "C":
+#         print("Good Performance")
+#     case "D":
+#         print("Needs Improvement")
+#     case "F":
+#         print("Failed")
+#     case _:
+#         print("Invalid Grade")
+
+# Q55
+# code = int(input("Enter Service Code:"))
+# match code:
+#     case 1:
+#         print("Check Balance")
+#     case 2:
+#         print("Recharge")
+#     case 3:
+#         print("Data Usage")
+#     case 4:
+#         print("Customer Support")
+#     case _:
+#         print("Invalid Service")
+
+# Q56
+# month = int(input("Month no.:"))
+# match month:
+#     case 1:
+#         print("January")
+#     case 2:
+#         print("February")
+#     case 3:
+#         print("March")
+#     case 4:
+#         print("April")
+#     case 5:
+#         print("May")
+#     case 6:
+#         print("June")
+#     case 7:
+#         print("July")
+#     case 8:
+#         print("August")
+#     case 9:
+#         print("September")
+#     case 10:
+#         print("October")
+#     case 11:
+#         print("November")
+#     case 12:
+#         print("December")
+#     case _:
+#         print("Invalid Month")
+
+# Q57
+# ext = input("Enter extension type:")
+# match ext:
+#     case "py":
+#         print("Python File")
+#     case "txt":
+#         print("Text File")
+#     case "pdf":
+#         print("PDF File")
+#     case "jpg":
+#         print("Image File")
+#     case _:
+#         print("Unknown File Type")
