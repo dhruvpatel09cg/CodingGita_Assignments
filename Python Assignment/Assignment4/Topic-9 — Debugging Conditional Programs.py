b5be9ad5f5260a5d35d80adeb3411b88b8925f04
Dhruv Patel <dhruv.patel.cg@gmail.com>
2026-09-25 21:37:29 +0530

    Topic 9 Assignment 04 completed

--- Python Assignment/Assignment4/Topic-9 — Debugging Conditional Programs.py
+++ Python Assignment/Assignment4/Topic-9 — Debugging Conditional Programs.py
@@ -1,1 +1,22 @@
+# Q69
+# age = int(input("Enter age: "))
 
+# if age >= 18:
+#     print("Eligible")
+# else:
+#     print("Not Eligible")
+
+# Q70
+# marks = int(input("Enter marks: "))
+
+# if marks >= 40:
+#     if marks >= 90:
+#         print("A")
+#     elif marks >= 75:
+#         print("B")
+#     else:
+#         print("Pass")
+# else:
+#     print("Fail")
+
+# Previously program hsn't any nested else statement resisting it to give output for marks in range 40 to 75
